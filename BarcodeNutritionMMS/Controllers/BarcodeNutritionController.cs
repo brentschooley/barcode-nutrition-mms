a98083022633a36703bde6d461dfc47b7cbe2e6a
brentschooley <schooleybrent@gmail.com>
2014-10-13 15:13:17 -0700

    Updated .gitignore

---
+++ BarcodeNutritionMMS/Controllers/BarcodeNutritionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BarcodeNutritionMMS.Controllers
+{
+    public class BarcodeNutritionController : Controller
+    {
+        // GET: BarcodeNutrition
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
